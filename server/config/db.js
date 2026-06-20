5c6232f3c77384c848ab7c79c18eba0410d0cee6
K Siddartha <kurmasiddartha0401@gmail.com>
2026-06-21 00:45:35 +0530

    fetch MONGO_URI from env only

--- server/config/db.js
+++ server/config/db.js
@@ -5,7 +5,11 @@ dns.setServers(['8.8.8.8', '1.1.1.1']);
 
 const connectDB = async () => {
     try {
-        const MONGO_URI = process.env.MONGO_URI || "mongodb+srv://taskmaster_user:taskmaster1234@cluster0.batjms1.mongodb.net/taskmaster?retryWrites=true&w=majority&appName=Cluster0";
+        const MONGO_URI = process.env.MONGO_URI;
+        if (!MONGO_URI) {
+            console.error("❌ MONGO_URI is not defined in environment variables");
+            process.exit(1);
+        }
 
         const conn = await mongoose.connect(MONGO_URI);
 
